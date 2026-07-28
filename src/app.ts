c9e3ec75feabbd49e11ab07341ef9436398ce0ff
zalish.shawon <zalish.shawon@gmail.com>
2026-07-28 22:43:32 +0600

    init

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from "express";
+
+
+const app: Application = express();
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Basic route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript + Express!');
+});
+
+
+export default app
