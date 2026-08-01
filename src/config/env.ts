f04d76c0c469f2e4f446a897db73a920c5adee74
zalish.shawon <zalish.shawon@gmail.com>
2026-08-01 21:29:51 +0600

    added env file

---
+++ src/config/env.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';    
+
+dotenv.config();
+
+interface EnvConfig {
+  NODE_ENV: string;
+  PORT: string;
+  DATABASE_URL: string;
+  BETTER_AUTH_SECRET: string;
+  BETTER_AUTH_URL: string;
+
+}
+
+const loadEnvVariables = (): EnvConfig => {
+    return {
+        NODE_ENV: process.env.NODE_ENV as string,
+        PORT: process.env.PORT as string,
+        DATABASE_URL: process.env.DATABASE_URL as string,
+        BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string
+    }
+}
+
+
+export const envConfig = loadEnvVariables();
